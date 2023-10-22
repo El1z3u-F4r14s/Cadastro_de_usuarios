29c6be1cfa3f6739fb60bc53c26ace4d214d2399
Elizeu Farias <122180679+El1z3u-F4r14s@users.noreply.github.com>
2023-10-22 01:23:59 -0400

    Upload de uma versão revisada do projeto
    Foi corrigido o erro que impedia a remoção de um CPF após consultá-lo.

---
+++ Projeto_de_cadastro.cpp
@@ -0,0 +1,183 @@
+#include <stdio.h> //biblioteca de comunicação com o usuário.
+#include <stdlib.h> //biblioteca de alocação de memória.
+#include <locale.h> //biblioteca de alocação de texto por região.
+#include <string.h> //biblioteca responsável por cuidar das strings.
+
+
+//FUNÇÃO DE CADASTRO
+int registro()
+{
+	setlocale(LC_ALL, "portuguese");
+	
+	//criação de variáveis/strings
+	char arquivo[40];
+	char cpf[40];
+	char nome[40];
+	char sobrenome[40];
+	char funcao[40];
+	//final da criação de varáveis
+	
+	printf("\t==========> CADASTRO DE ALUNO <==========\n\n");
+	printf("Digite o CPF: "); //coleta do CPF a ser cadastrado
+	scanf("%s", cpf); //armazenando o dado coletado na string de CPF
+	
+	strcpy(arquivo, cpf); //responsável por criar os valores das strings
+	FILE *file; //para criar o arquivo
+	file = fopen(arquivo, "w"); //criando o arquivo e escrevendo nele com "w"
+	fprintf(file,cpf); //para salvar a informação dentro do arquivo
+	fclose(file); //fechando o arquivo
+	file = fopen(arquivo, "a"); //abrindo o arquivo e atualizando com "a"
+	fprintf(file,", ");
+	fclose(file); //fechando o arquivo atualizado
+	
+	printf("Digite o nome: ");
+	scanf("%s",nome);
+	file = fopen(arquivo, "a");
+	fprintf(file,nome);
+	fclose(file);
+	file = fopen(arquivo, "a");
+	fprintf(file,", ");
+	fclose(file);
+	
+	printf("Digite o sobrenome: ");
+	scanf("%s",sobrenome);
+	file = fopen(arquivo, "a");
+	fprintf (file,sobrenome);
+	fclose(file);
+	file = fopen(arquivo, "a");
+	fprintf(file, ", ");
+	fclose(file);
+	
+	printf("Digite a função: ");
+	scanf("%s",funcao);
+	file = fopen(arquivo, "a");
+	fprintf(file,funcao);
+	fclose(file);
+	file = fopen(arquivo, "a");
+	fprintf(file, ". ");
+	fclose(file);
+		
+	system ("pause"); //congelar a tela
+}
+
+//FUNÇÃO DE CONSULTAS
+int consulta()
+{
+	setlocale(LC_ALL, "portuguese");
+	
+	//criando variáveis
+	char cpf[40];
+	char conteudo[200];
+	char *token; //variável para dividir as strings em linhas.
+		
+	printf("\t==========> CONSULTA DE ALUNO <==========\n\n");
+	printf("Digite o CPF que deseja consultar: ");
+	scanf("%s",cpf);
+	FILE *file;
+	file = fopen(cpf, "r"); //abrindo e lendo o arquivo com "r"
+		
+	if(file == NULL) //função responsável por exibir a tela de erro caso o usuário digite um CPF inexistente
+	{
+		printf("\nCPF não localizado!\n\n");
+	}
+	
+	while(fgets(conteudo, 200, file) != NULL)
+	{
+		printf("\n\t==========> RESULTADO DA BUSCA <==========\n\n");
+		
+		token = strtok(conteudo, ","); //variável para dividir o resultado da consulta em linhas.
+		printf("CPF: %s\n", token); //informação para aparecer antes dos dados do CPF.
+		
+		token = strtok(NULL, ",");
+		printf("NOME: %s\n", token);
+		
+		token = strtok(NULL, ",");
+		printf("SOBRENOME: %s\n", token);
+		
+		token = strtok(NULL, ".");
+		printf("FUNÇÃO: %s", token);
+		
+		printf("\n\n");		
+	}
+	system ("pause"); //congelando a tela
+	fclose(file); //fechando o arquivo que tinha os resultados da busca
+}
+
+//FUNÇÃO PARA DELETAR
+int deletar()
+{
+	setlocale(LC_ALL, "portuguese");
+	
+	//criando variável
+	char cpf[40];
+	
+	printf("\t==========> DELETAR ALUNO <==========\n\n");
+	printf("Digite o CPF a ser deletado: ");
+	scanf("%s", cpf);
+			
+	FILE *file;
+	file = fopen(cpf, "r");
+	fclose(file);
+	
+	if(file == NULL)
+	{
+		printf("\nCPF não localizado!\n\n");
+		system ("pause");
+		return 0;
+	}
+	
+	if(remove(cpf) == 0){
+		printf("\nCPF deletado com sucesso!\n\n");
+	}else {
+		printf("\nNão foi possível remover o CPF!\n\n");
+	}
+	
+	system ("pause");
+		
+}
+
+//FUNÇÃO DA TELA INICIAL
+
+int main()
+{
+	int opcao=0; //definindo variável para inteiro.
+	int laco=1;
+	
+	for(laco=1;laco=1;)
+	{
+		system("cls");
+		setlocale(LC_ALL, "portuguese"); //definindo a linguagem.
+	
+		//tela inicial
+		printf("\t==========> CARTÓRIO DA EBAC <==========\n\n");
+		printf("\tDIGITE A OPÇÃO DESEJADA:\n\n");
+		printf("\t1 - CADASTRAR ALUNO\n\t2 - CONSULTAR ALUNO\n\t3 - DELETAR ALUNO\n\n");
+		printf("\tOPÇÃO:\n");
+		//fim da tela inicial
+	
+		scanf("%d", &opcao); //armazenando a escolha do usuário (nesse caso inteiro ["%d"]
+	
+		system("cls"); //função para limpar a tela.
+	
+		//inicio da seleção.
+		switch(opcao)
+		{
+			case 1:
+			registro(); //chamada da função de cadastro
+			break;
+			
+			case 2:
+			consulta(); //chamada da função de consulta
+			break;
+			
+			case 3:
+			deletar(); //chamada da função de deletar
+			break;
+			
+			default:					
+			printf("\tOpção inválida. Insira uma opção válida.\n\n");
+			system("pause");
+			break;
+		}//fim da seleção.		
+	}	
+}
